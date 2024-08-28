683667fcdc1401c8e5d8a523d0c8707ece154497
Eduardo Escares <eduardoscrs60@gmail.com>
2024-08-28 00:52:25 -0400

    firebase pre-sprint 1.1

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import { pool } from './config/db.js'; // Asegúrate de que la ruta sea correcta
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { pool } from './config/db.js'; // Asegúrate de que la ruta sea correcta
 
-// dotenv.config();
+dotenv.config();
 
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-// // Prueba de conexión a la base de datos
-// pool
-//   .getConnection()
-//   .then((connection) => {
-//     console.log('Conexión a la base de datos exitosa');
-//     connection.release(); // Liberar la conexión
-//   })
-//   .catch((err) => {
-//     console.error('Error al conectar a la base de datos:', err.message);
-//   });
+// Prueba de conexión a la base de datos
+pool
+  .getConnection()
+  .then((connection) => {
+    console.log('Conexión a la base de datos exitosa');
+    connection.release(); // Liberar la conexión
+  })
+  .catch((err) => {
+    console.error('Error al conectar a la base de datos:', err.message);
+  });
 
-// export default app;
+export default app;
--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,18 @@
-// import { createPool } from 'mysql2/promise';
-// import dotenv from 'dotenv';
+import { query } from 'express';
+import admin from 'firebase-admin';
+import { QuerySnapshot } from 'firebase-admin/firestore';
+import serviceAccount from '../../serviceAccountKeys.js' ; // Asegúrate de que la ruta sea correcta
 
-// dotenv.config();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-// // Conexión a la base de datos
-// export const pool = createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   port: parseInt(process.env.DB_PORT),
-//   database: process.env.DB_DATABASE,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 30000,
-// });
+const db = admin.firestore();
+
+let customersRef = db.collection('Customers');
+
+customersRef.get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, ' => ', doc.data());
+  });
+});

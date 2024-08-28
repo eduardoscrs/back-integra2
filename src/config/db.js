07057260625f0a3af954fe4d921b97496f32e4f5
Eduardo Escares <eduardoscrs60@gmail.com>
2024-08-27 23:39:27 -0400

    firebase pre sprint1

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,17 @@
-import { createPool } from 'mysql2/promise';
-import dotenv from 'dotenv';
+// import { createPool } from 'mysql2/promise';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-// Conexión a la base de datos
-export const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT),
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 30000,
-});
+// // Conexión a la base de datos
+// export const pool = createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   port: parseInt(process.env.DB_PORT),
+//   database: process.env.DB_DATABASE,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   connectTimeout: 30000,
+// });

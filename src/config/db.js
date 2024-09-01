95d9b9308ac55ad613106d54f0d7472d127ad53c
Eduardo Escares <eduardoscrs60@gmail.com>
2024-08-31 21:17:54 -0400

    conexión a sql

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,33 @@
-import { query } from 'express';
-import admin from 'firebase-admin';
-import { QuerySnapshot } from 'firebase-admin/firestore';
-import serviceAccount from '../../serviceAccountKeys.js'; // Asegúrate de que la ruta sea correcta
+// import { query } from 'express';
+// import admin from 'firebase-admin';
+// import { QuerySnapshot } from 'firebase-admin/firestore';
+// import serviceAccount from '../../serviceAccountKeys.js'; // Asegúrate de que la ruta sea correcta
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-const db = admin.firestore();
+// const db = admin.firestore();
 
-let customersRef = db.collection('Customers');
+// let customersRef = db.collection('Customers');
 
-customersRef.get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, ' => ', doc.data());
-  });
+// customersRef.get().then((querySnapshot) => {
+//   querySnapshot.forEach((doc) => {
+//     console.log(doc.id, ' => ', doc.data());
+//   });
+// });
+
+import { createPool } from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Conexión a la base de datos
+export const pool = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT),
 });

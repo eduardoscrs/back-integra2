051fa7a0c2ae9709d8cc69ee8a754aaf7e588817
NicolasVZ86 <nvalenzuela2022@alu.uct.cl>
2024-09-02 04:06:02 -0400

    CRUD Usuarios 50%

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { pool } from './config/db.js'; // Asegúrate de que la ruta sea correcta
+// import { pool } from './config/db.js'; // Asegúrate de que la ruta sea correcta
+import userroute from './routes/userroute.js';
 
 dotenv.config();
 
@@ -9,15 +10,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/user', userroute);
+
+
 // Prueba de conexión a la base de datos
-pool
-  .getConnection()
-  .then((connection) => {
-    console.log('Conexión a la base de datos exitosa');
-    connection.release(); // Liberar la conexión
-  })
-  .catch((err) => {
-    console.error('Error al conectar a la base de datos:', err.message);
-  });
+// pool
+//   .getConnection()
+//   .then((connection) => {
+//     console.log('Conexión a la base de datos exitosa');
+//     connection.release(); // Liberar la conexión
+//   })
+//   .catch((err) => {
+//     console.error('Error al conectar a la base de datos:', err.message);
+//   });
 
 export default app;

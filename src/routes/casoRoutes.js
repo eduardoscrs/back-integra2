d9afdc74cbcab9570391bcc594effdd52167dd35
Eduardo Escares <eduardoscrs60@gmail.com>
2024-09-18 16:02:22 -0300

    a

--- src/routes/casoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const casoController = require('../controllers/casoController');
-
-router.post('/casos', casoController.crearCaso);
-
-module.exports = router;

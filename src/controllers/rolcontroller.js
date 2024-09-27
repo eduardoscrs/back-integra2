0139beaa5cedafd7802b2135341186c28624127d
Eduardo Escares <eduardoscrs60@gmail.com>
2024-09-27 14:44:58 -0300

    CRUD
    Revisar crud con nueva BD

--- src/controllers/rolcontroller.js
+++ src/controllers/rolcontroller.js
@@ -1,36 +1,36 @@
-import { pool } from "../config/db.js";
+import { pool } from '../config/db.js';
 
 export const getRol = async (req, res) => {
-    try {
-        const conn = await pool.getConnection();
-        const roles = await conn.query('SELECT * FROM ROLES');
-        res.json(roles[0]);
-    } catch (error) {
-        console.log("algo salio mal");
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const conn = await pool.getConnection();
+    const roles = await conn.query('SELECT * FROM ROLES');
+    res.json(roles[0]);
+  } catch (error) {
+    console.log('algo salio mal');
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const registerRol = async (req, res) => {
-    try {
-        const { 
-            nombre,
-            puede_editar_precios, 
-            puede_ver_casos,
-            puede_generar_excel,
-            puede_generar_informe_pdf
-            } = req.body;
-        const newRole = {   
-            nombre,
-            puede_editar_precios,
-            puede_ver_casos,
-            puede_generar_excel,
-            puede_generar_informe_pdf
-            };
-        const conn = await pool.getConnection();
-        conn.query('INSERT INTO ROLES SET ?', [newRole]);
-        res.json(newRole);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const {
+      nombre,
+      puede_editar_precios,
+      puede_ver_casos,
+      puede_generar_excel,
+      puede_generar_informe_pdf,
+    } = req.body;
+    const newRole = {
+      nombre,
+      puede_editar_precios,
+      puede_ver_casos,
+      puede_generar_excel,
+      puede_generar_informe_pdf,
+    };
+    const conn = await pool.getConnection();
+    conn.query('INSERT INTO ROLES SET ?', [newRole]);
+    res.json(newRole);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- src/routes/userroute.js
+++ src/routes/userroute.js
@@ -1,13 +1,17 @@
 import express from 'express';
-import { getUsers ,registerUser, editUser } from '../controllers/usercontroller.js';
+import {
+  getUsers,
+  registerUser,
+  editUser,
+} from '../controllers/usercontroller.js';
 import { getRol, registerRol } from '../controllers/rolcontroller.js';
 
 const router = express.Router();
 
 router.post('/registerUser', registerUser);
 router.get('/getUser', getUsers);
-router.post('/registerRol', registerRol)
-router.get('/getRol', getRol)
-router.put('/editUser', editUser)
+router.post('/registerRol', registerRol);
+router.get('/getRol', getRol);
+router.put('/editUser', editUser);
 
 export default router;

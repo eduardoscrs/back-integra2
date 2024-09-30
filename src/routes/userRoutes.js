d97501679a6f6695d5b2d52eb16bf53e921fc4d1
Eduardo Escares <eduardoscrs60@gmail.com>
2024-09-30 16:52:34 -0300

    update

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,20 +1,18 @@
 import express from 'express';
 import {
-  getUsers,
   registerUser,
+  getUsers,
   editUser,
+  registerRol,
+  getRol,
 } from '../controllers/userController.js';
-import { getRol, registerRol } from '../controllers/rolController.js';
+
 
 const router = express.Router();
 
-// Rutas para usuarios
-router.post('/register', registerUser); // Registro de usuario
-router.get('/', getUsers); // Obtener usuarios
-router.put('/:id', editUser); // Editar usuario por ID
-
-// Rutas para roles
-router.post('/roles', registerRol); // Registro de rol
-router.get('/roles', getRol); // Obtener roles
-
-export default router;
+router.post('/register', registerUser);
+router.get('/users', getUsers);
+router.put('/edit/:id', editUser);
+// Actualizar por rol
+router.post('/registerRol', registerRol);
+router.get('/rol', getRol);

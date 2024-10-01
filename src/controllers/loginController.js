5c01daff587ffab97ae1877f6f2e10c61fb55c58
Eduardo Escares <eduardoscrs60@gmail.com>
2024-09-30 22:34:57 -0300

    prueba login

---
+++ src/controllers/loginController.js
@@ -0,0 +1,30 @@
+import { pool } from '../config/db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const [rows] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+
+    const user = rows[0];
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Contraseña incorrecta' });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({ token });
+  } catch (err) {
+    console.error('Error al iniciar sesión:', err.message);
+    res.status(500).json({ message: 'Error al iniciar sesión' });
+  }
+};

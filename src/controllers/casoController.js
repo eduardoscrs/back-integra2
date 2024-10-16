41706ffe7d919f36644547857ae74b7dc8aa51ad
ConstanzaOyarzo <coyarzod0@gmail.com>
2024-10-16 14:52:27 -0300

    Cambiado para coincidir con back-end
    + Cambio de nombres de variables para que funcione correctamente con el back-end.

--- src/controllers/casoController.js
+++ src/controllers/casoController.js
@@ -247,22 +247,22 @@ export const getCasoById = async (req, res) => {
 
 export const actualizarEstadoCaso = async (req, res) => {
   const { id } = req.params;
-  const { ID_estado } = req.body;
+  const { estado } = req.body;
 
-  // Validar que ID_estado no sea nulo o undefined
-  if (ID_estado === null || ID_estado === undefined) {
+  // Validar que el estado no sea nulo o undefined
+  if (estado === null || estado === undefined) {
     return res.status(400).json({ mensaje: 'El campo ID_estado es requerido' });
   }
 
-  // Validar que ID_estado sea un valor válido (en este caso, entre 1 y 4)
-  if (![1, 2, 3, 4].includes(ID_estado)) {
+  // Validar que el estado sea un valor válido (en este caso, entre 1 y 4)
+  if (![1, 2, 3, 4].includes(estado)) {
     return res.status(400).json({ mensaje: 'ID de estado no válido' });
   }
 
   try {
     const [result] = await pool.query(
       `UPDATE Caso SET ID_estado = ? WHERE ID_caso = ?`,
-      [ID_estado, id]
+      [estado, id]
     );
 
     if (result.affectedRows === 0) {
